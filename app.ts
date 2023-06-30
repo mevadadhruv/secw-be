9541901f0bc429d795af3d745cc135abd73ef66f
dhruv.m@dntinfotech.com <dhruv.m@dntinfotech.com>
2023-06-30 14:25:06 +0530

    remove extra app file

--- app.ts
+++ app.ts
@@ -1,29 +1,28 @@
-import express,{Application} from "express";
+import express, { Application } from "express";
 import "reflect-metadata";
-import bodyParser,{ json } from "body-parser";
-import { iocContainer as container } from "./src/config/container";
+import bodyParser, { json } from "body-parser";
 import { DatabaseConnection } from "./src/config/db";
 import index from "./src/routes/index";
-import passport from "passport";
 import session from "express-session";
-import {} from "./src/controllers/AuthController";
-import {config} from "./src/config/env";
+import { config } from "./src/config/env";
 
-const app:Application = express();
+const app: Application = express();
 
-app.use(session({
-    resave : false,
-    saveUninitialized : false,
-    secret : "SECRETKEY"
-}));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "SECRETKEY",
+  })
+);
 
 app.use(json());
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 DatabaseConnection();
 
-app.use(index.router,index.profileRouter,index.AuthRouter);
+app.use(index.router, index.profileRouter, index.AuthRouter);
 
-app.listen(config.PORT, ():void => {
-    console.log(`server running on  ${config.PORT}`);
+app.listen(config.PORT, (): void => {
+  console.log(`server running on  ${config.PORT}`);
 });
