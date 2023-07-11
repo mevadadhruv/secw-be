52462168b761ad2a488d708b70a4439367133652
Dhruv Mevada <dhruv.m@dntinfotech.com>
2023-07-11 10:48:23 +0530

    extra things

--- app.ts
+++ app.ts
@@ -1,22 +1,25 @@
-import express, { Application } from 'express';
-import * as dotenv from 'dotenv';
-import 'reflect-metadata';
-import bodyParser, { json } from 'body-parser';
-import { DatabaseConnection } from './src/config/db';
-import index from './src/routes/index';
-import { config } from './src/config/env';
-import cors from 'cors';
+import express, { Application } from "express";
+import * as dotenv from "dotenv";
+import "reflect-metadata";
+import bodyParser, { json } from "body-parser";
+import { DatabaseConnection } from "./src/config/db";
+import index from "./src/routes/index";
+import { config } from "./src/config/env";
+import cors from "cors";
 
 dotenv.config();
 const port = config.PORT;
 const app: Application = express();
 
-app.use(cors({ origin: '*' }));
-app.options('*', cors({ origin: '*' }));
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
+app.use(cors({ origin: "*" }));
+app.options("*", cors({ origin: "*" }));
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use(json());
@@ -25,11 +28,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 DatabaseConnection();
 
 app.use(index.router);
-app.use('/profile', index.profileRouter);
-app.use('/document', index.DocumentRouter);
-app.use('/vendor', index.VendorRouter);
-app.use('/role', index.roleRouter);
+app.use("/profile", index.profileRouter);
+app.use("/document", index.DocumentRouter);
+app.use("/vendor", index.VendorRouter);
+app.use("/role", index.roleRouter);
 
 app.listen(port, (): void => {
-	console.log(`server running on  ${port}`);
+  console.log(`server running on  ${port}`);
 });
