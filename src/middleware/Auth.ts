ac40f2f7162a55041fc8425367cbc6f2b0d7797d
Bhakti12 <sanghanibhakti922@gmail.com>
2023-08-03 10:40:20 +0530

    chages in middleware

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -14,15 +14,16 @@ export const verifyToken = async (
 ) => {
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
-  console.log(token);
+  if(token){
+    try {
+      const decoded = jwt.verify(token, secretKey);
+      req.user = decoded;
+    } catch (err) {
+      next(new appError("invalid token", 402));
+    }
+  }
   if (!token) {
     next(new appError("PLease Login First to continue", 403));
-  }
-  try {
-    const decoded = jwt.verify(token, secretKey);
-    req.user = decoded;
-  } catch (err) {
-    next(new appError("invalid token", 402));
   }
   return next();
 };
